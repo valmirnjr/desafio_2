04e37cc63f3aba36ad26b687f207247773bea547
valminjr <junior.valmir6@gmail.com>
2019-07-22 21:24:44 -0300

    feature: Avatar de usuário

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import multer from "multer";
 
 import UserController from "./app/controllers/UserController";
 import SessionController from "./app/controllers/SessionController";
+import FileController from "./app/controllers/FileController";
 import authMiddleware from "./app/middlewares/auth";
 
 import multerConfig from "./config/multer";
@@ -17,9 +18,7 @@ routes.post("/session", SessionController.store);
 
 routes.use(authMiddleware);
 
-routes.post("/files", upload.single("file"), (req, res) => {
-  return res.json({ ok: true });
-});
+routes.post("/files", upload.single("file"), FileController.store);
 
 routes.put("/users", UserController.update);
 
